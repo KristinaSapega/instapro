96dea9bf7b5a33aeeb49340ceceed7ea6e123b29
Kristina <128722669+KristinaSapega@users.noreply.github.com>
2024-10-08 00:37:10 +0300

    реализована страница добавления поста

--- api.js
+++ api.js
@@ -68,3 +68,26 @@ export function uploadImage({ file }) {
     return response.json();
   });
 }
+
+export function addPost({ token, description, imageUrl }) {
+  return fetch(`${postsHost}`+"/", {
+    method: "POST",
+    headers: {
+      Authorization: token,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      description,
+      imageUrl,
+    }),
+  })
+  .then((response) => {
+    if (response.status === 401) {
+      throw new Error("Нет авторизации");
+    }
+    return response.json();
+  })
+  .then((data) => {
+    return data.post;
+  });
+}
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { getPosts } from "./api.js";
+import { addPost, getPosts } from "./api.js";
 import { renderAddPostPageComponent } from "./components/add-post-page-component.js";
 import { renderAuthPageComponent } from "./components/auth-page-component.js";
 import {
@@ -110,12 +110,27 @@ const renderApp = () => {
     return renderAddPostPageComponent({
       appEl,
       onAddPostClick({ description, imageUrl }) {
-        // TODO: реализовать добавление поста в API
-        console.log("Добавляю пост...", { description, imageUrl });
-        goToPage(POSTS_PAGE);
+        const token = getToken();
+        
+        if (!token) {
+          alert("Необходимо авторизоваться для добавления поста.");
+          goToPage(AUTH_PAGE);
+          return;
+        }
+        
+        addPost({ token, description, imageUrl })
+          .then((newPost) => {
+            console.log("Пост добавлен успешно:", newPost);
+            goToPage(POSTS_PAGE); // Возврат на страницу постов после добавления
+          })
+          .catch((error) => {
+            console.error("Ошибка при добавлении поста:", error);
+            alert("Ошибка при добавлении поста. Попробуйте еще раз.");
+          });
       },
     });
   }
+  
 
   if (page === POSTS_PAGE) {
     return renderPostsPageComponent({
